4e7bd366cdfc3587c2844bf5a8292346b42b08c1
rakeshgnanendra <rakeshgnanendra@gmail.com>
2026-04-10 17:08:59 +0530

    cors Error fix

--- src/main.ts
+++ src/main.ts
@@ -13,13 +13,32 @@ async function bootstrap() {
     },
   )
 
-  app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'https://taskanalyticsdp.netlify.app',
-    ],
-    credentials: true,
-  })
+app.enableCors({
+  origin: [
+    'http://localhost:5173',
+    'https://taskanalyticsdp.netlify.app',
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type, Authorization',
+  credentials: true,
+});
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+  );
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
   await app.listen(process.env.PORT ?? 3000)
 }
