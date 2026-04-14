119da7b64f1af4c94d9c3a41f502203f7e2255ad
rakeshgnanendra <rakeshgnanendra@gmail.com>
2026-04-14 16:04:59 +0530

    Task Notification fix

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,22 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import * as nodemailer from 'nodemailer';
+import { Resend } from 'resend';
 
 @Injectable()
 export class EmailService {
- private transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+  private resend = new Resend(process.env.RESEND_API_KEY);
 
   async sendMail(to: string, subject: string, text: string) {
     try {
-      await this.transporter.sendMail({
-        from: `"Task Analytics" <${process.env.EMAIL_USER}>`,
+      await this.resend.emails.send({
+        from: 'onboarding@resend.dev', // default (works immediately)
         to,
         subject,
         text,
